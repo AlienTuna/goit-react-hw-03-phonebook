ca10625c0222185a7d8d4ff8eca1eef51dfe65ff
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-07-18 00:06:03 +0300

    GRIP-2 PHONEBOOK

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import { nanoid } from "nanoid";
+
+import { ContactForm } from "./ContactForm";
+import { ContactList } from './ContactList';
+import { Filter } from "./Filter";
+
+import { Container, SectionStyled } from "./App.styled";
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  addContact = (name, number) => {
+    const id = nanoid(3);
+    this.setState(prevState => ({
+      contacts: ([...prevState.contacts, { id, name, number }])
+    }))
+  }
+
+  deleteContact = id => {
+    this.setState(state => ({
+      contacts: (state.contacts.filter(contact => contact.id !== id))
+    }))
+  }
+
+  setFilter = (e) => {
+    this.setState({filter: e.target.value});
+  }
+
+  filterContactsByName = () => {
+    const {contacts, filter} = this.state;
+    return contacts.filter(contact => 
+      contact.name.toLowerCase().includes(filter.toLowerCase()))
+    }
+
+  render() {
+    const { filter } = this.state;
+
+    return (
+      <SectionStyled>
+        <h1>Phonebook</h1>
+        <Container>
+          <h2>Add new contact</h2>
+          <ContactForm
+            onAddContact={this.addContact} />
+        </Container>
+
+        <Container>
+          <h2>Contacts</h2>
+            <Filter 
+            filterString={filter}
+            onSetFilter={this.setFilter}
+            />
+          <ContactList
+            list={this.filterContactsByName()}
+            onDeleteContact={this.deleteContact}
+          />
+        </Container>
+      </SectionStyled>
+    )
+  }
 };
