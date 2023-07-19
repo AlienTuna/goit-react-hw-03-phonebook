1ceb6de0f72ab76107091d3ea13096d72c95db15
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-07-19 09:53:59 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export function Filter({filterString, onSetFilter}) {
     return (
         <label name="filter">
-            {/* <h3>Find contact by name</h3> */}
+            <h3>Find contact by name</h3>
             <InputStyled
             name="filter"
             type="text"
